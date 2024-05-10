c1fa3edb0c4c54eade10ec4adc7865908a696584
Farhan Akthar <farhanakthar2003@gmail.com>
2024-05-10 07:53:51 +0530

    Third commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 scaler=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',prediction_text="")
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -20,6 +20,16 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    rounded_output = round(output, 3)  # Round to 3 decimal points
+    # print(output[1])
+    return render_template("home.html", prediction_text="The House price prediction is {:.3f}".format(rounded_output)) 
 
 if __name__=="__main__":
     app.run(debug=True)
